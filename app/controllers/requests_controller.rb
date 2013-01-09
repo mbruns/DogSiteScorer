a317282b9b1d3b6f487f1055c2ab5359975a9591
Meike Bruns <meike-bruns@hotmail.de>
2013-01-09 15:12:38 +0100

    switched flag

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,7 +3,7 @@ class RequestsController < ApplicationController
   def show
     @request = Request.find(params[:id])
     #@file = "/home/meike/DoGSiteScorer/tmp/cache/#{@request.pdb}"
-    @ready = false
+    @ready = true
     if @ready == true
       render :text => "<pre>#{File.read("/home/meike/DoGSiteScorer/tmp/cache/#{@request.pdb}")}</pre>"
     end
